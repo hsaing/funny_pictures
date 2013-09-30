25c5d620c00bf62dcaa4b35ac86a6adb92f90bb0
H.Saing <hsaing@aol.com>
2013-09-30 11:09:09 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FunnyPictures::Application.config.secret_key_base = '2a85e868d9bd9db7ee3a07371bd73f821d14a749fc24e2a2d2b3913aa4c08da2ff5d24e50c3259d460803545680cdfe7794208985644c942fc517f8e5a54e2d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FunnyPictures::Application.config.session_store :cookie_store, key: '_funny_pictures_session'
