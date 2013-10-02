f46ae9d93f836f2dfc8ffa39272947a5dcc1ebe2
H.Saing <hsaing@aol.com>
2013-10-01 18:20:24 -0700

    Add Pin Associations

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,31 +1,34 @@
 class PinsController < ApplicationController
-  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except: [:index]
+  # before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
   # GET /pins
   # GET /pins.json
   def index
-    @pins = Pin.all
+    @pin = Pin.all
   end
 
   # GET /pins/1
   # GET /pins/1.json
   def show
+    @pin = Pin.find(params[:id])
   end
 
   # GET /pins/new
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.new
   end
 
   # GET /pins/1/edit
   def edit
+    @pin = current_user.pins.find(params[:id])
   end
 
   # POST /pins
   # POST /pins.json
   def create
-    @pin = Pin.new(pin_params)
-
+    @pin = current_user.pins.new(params[:pin])
+    
     respond_to do |format|
       if @pin.save
         format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
@@ -40,6 +43,8 @@ class PinsController < ApplicationController
   # PATCH/PUT /pins/1
   # PATCH/PUT /pins/1.json
   def update
+    @pin = current_user.pins.find(params[:id])
+
     respond_to do |format|
       if @pin.update(pin_params)
         format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
@@ -54,21 +59,12 @@ class PinsController < ApplicationController
   # DELETE /pins/1
   # DELETE /pins/1.json
   def destroy
+    @pin = current_user.pins.find(params[:id])
     @pin.destroy
+
     respond_to do |format|
       format.html { redirect_to pins_url }
       format.json { head :no_content }
     end
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pin
-      @pin = Pin.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def pin_params
-      params.require(:pin).permit(:description)
-    end
 end
